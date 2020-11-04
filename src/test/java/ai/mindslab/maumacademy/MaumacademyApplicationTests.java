74c29f7fbdd1e52920fc816e5d2c7eb377a23d1b
Sang Min Park <9714park@gmail.com>
2020-11-03 21:22:11 -0500

    Update property files.

--- src/test/java/ai/mindslab/maumacademy/MaumacademyApplicationTests.java
+++ src/test/java/ai/mindslab/maumacademy/MaumacademyApplicationTests.java
@@ -1,13 +1,13 @@
-package ai.mindslab.maumacademy;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MaumacademyApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package ai.mindslab.maumacademy;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MaumacademyApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
